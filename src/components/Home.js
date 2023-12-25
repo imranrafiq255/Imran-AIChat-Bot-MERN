9118812326fcced6f907938f8549590abe401b21
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2023-12-26 04:27:00 +0500

    made some changes on 26-12-2023

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
   const [text, setText] = useState("");
   const [generatedAnswer, setAnswer] = useState("");
   const [loader, setLoader] = useState(false);
-  const API_KEY = process.env.key;
+  const API_KEY = process.env.REACT_APP_KEY;
   const apiUrl =
     "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" +
     API_KEY;
